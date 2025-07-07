c04b3169796f105c25f5692fea4302fc58015846
Gabriel Maccari <gabrielmaccari5@gmail.com>
2025-07-06 23:24:38 -0300

    new project

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone xr = new Iphone("99999999999", "XR", "1234567890", 64);
+xr.InstalarAplicativo("WhatsApp");
+xr.Ligar();
+xr.ReceberLigacao();
+
+Nokia nokia = new Nokia("88888888888", "3310", "0987654321", 16);
+nokia.InstalarAplicativo("Snake");
+nokia.Ligar();
+nokia.ReceberLigacao();
